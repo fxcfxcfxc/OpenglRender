458dc3432b020e441295dc8ecaff2edc39f36cc9
fxcfxcfxc <4330822@qq.com>
2022-06-13 01:15:05 +0800

    add

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ float vertices[] =
 {
     0.5f,0.5f,0.0f,
     0.5f,-0.5f,0.0f,
-    -0.0f,-0.5f,0.0f,
+    -0.5f,-0.5f,0.0f,
     - 0.5f,0.5f,0.0f
 
 };
@@ -84,8 +84,8 @@ int main()
     ////----------------------------数据copy buffer--------
     //设置渲染窗口的大小
     glViewport(0,0,800,600);
-
-
+    //glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+    
     //生成VAO对象，VAO就像是属性列表
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
@@ -101,7 +101,7 @@ int main()
 
 
 
-    //创建EBO
+    //创建EBO对象
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
@@ -147,12 +147,19 @@ int main()
         //清空屏幕的color buffer
         glClear(GL_COLOR_BUFFER_BIT);
 
+        //绑定上下文VAO
+        glBindVertexArray(VAO);
 
-        glBindVertexArray(VAO);       
+        //绑定EBO到   GL_ELEMENT_ARRAY_BUFFER
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
         //当我们渲染一个物体时要使用的着色器程序
         glUseProgram(shaderProgram);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        //第一个参数绘制模式，第二个参数绘制定点数，第三个参数是索引类型，第四个参数 EBO中的偏移量
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
+        
+        
         //
         glfwSwapBuffers(window);
         glfwPollEvents();
